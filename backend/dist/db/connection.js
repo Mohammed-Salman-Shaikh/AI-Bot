f9b9b34c90fad333499b32d27a89b6441f8b45f7
Mohammed Salman Shaikh <salluarsh.786@gmail.com>
2023-11-14 17:19:21 +0530

    First commit

---
+++ backend/dist/db/connection.js
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose";
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    }
+    catch (error) {
+        console.log("Error connecting to the database: ", error);
+        throw new Error("Cannot connect to mongodb");
+    }
+}
+async function disconnectFromDatabse() {
+    try {
+        await disconnect();
+    }
+    catch (error) {
+        console.log("Error connecting to the database: ", error);
+        throw new Error("Cannot disconnect to mongodb");
+    }
+}
+export { connectToDatabase, disconnectFromDatabse };
+//# sourceMappingURL=connection.js.map
